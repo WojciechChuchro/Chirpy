cd0be19c90acf395885a9c88a10bc9246bfefd2b
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-04-22 19:41:37 +0200

    feat: implement basic http server

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type apiHandler struct{}
+
+func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
+
+func main() {
+	mux := http.NewServeMux()
+	var srv http.Server
+	srv.Addr = ":8080"
+	srv.ListenAndServe()
+	mux.Handle("/api/", apiHandler{})
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		// The "/" pattern matches everything, so we need to check
+		// that we're at the root here.
+		if req.URL.Path != "/" {
+			http.NotFound(w, req)
+			return
+		}
+		fmt.Fprintf(w, "Welcome to the home page!")
+	})
+}
