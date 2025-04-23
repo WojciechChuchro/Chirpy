c33f94b7201b5480c517bddce0e48e5a330df48c
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-04-23 15:36:10 +0200

    feat: implement three routes

--- main.go
+++ main.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 type apiHandler struct{}
 
-func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
+func (apiHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(w, req)
+		return
+	}
+	fmt.Fprintf(w, "Welcome to the home page!")
+}
 
 func main() {
+	port := ":8080"
 	mux := http.NewServeMux()
-	var srv http.Server
-	srv.Addr = ":8080"
-	srv.ListenAndServe()
-	mux.Handle("/api/", apiHandler{})
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		// The "/" pattern matches everything, so we need to check
-		// that we're at the root here.
-		if req.URL.Path != "/" {
-			http.NotFound(w, req)
-			return
-		}
-		fmt.Fprintf(w, "Welcome to the home page!")
+	mux.HandleFunc("/app", func(w http.ResponseWriter, req *http.Request) {
+		http.ServeFile(w, req, "./index.html")
 	})
+	mux.HandleFunc("/app/assets", func(w http.ResponseWriter, req *http.Request) {
+		http.ServeFile(w, req, "./index.html")
+		w.Write([]byte(""))
+	})
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	//http.FileServer(http.Dir("."))
+	log.Printf("Server started on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(":8080", mux))
+	// mux.Handle("/", apiHandler{})
 }
