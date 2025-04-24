df971c85ef8e5c4af72629ad1003742acefb20a1
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2025-04-24 20:35:07 +0200

    feat: implement json parsing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -44,15 +45,95 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})))
-	mux.Handle("GET /api/metrics", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	mux.Handle("GET /admin/metrics", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(fmt.Sprintf("Hits: %d", apiCfg.fileserverHits.Load())))
+		w.Header().Add("Content-Type", "text/html")
+		http.ServeFile(w, req, "./metrics.html")
+		w.Write([]byte(fmt.Sprintf("<p>Chirpy has been visited %d times!</p>", apiCfg.fileserverHits.Load())))
 	}))
-	mux.Handle("POST /api/reset", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	mux.Handle("POST /admin/reset", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		apiCfg.fileserverHits.Store(0)
+	}))
+	mux.Handle("POST /api/validate_chirp", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		type parameters struct {
+			Body string `json:"body"`
+		}
+		decoder := json.NewDecoder(req.Body)
+		params := parameters{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			return500(w)
+			return
+		}
+		log.Printf("len: %d", len(params.Body))
+		if len(params.Body) > 140 {
+			lengthError(w)
+			return
+		}
+		success(w)
 		apiCfg.fileserverHits.Store(0)
 	}))
 	log.Printf("Server started on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 	// mux.Handle("/", apiHandler{})
+}
+
+func return500(w http.ResponseWriter) {
+
+	type returnVals struct {
+		Error string `json:"error"`
+	}
+	respBody := returnVals{
+		Error: "Something went wrong",
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(500)
+	w.Write(dat)
+	return
+}
+
+func lengthError(w http.ResponseWriter) {
+
+	type returnVals struct {
+		Error string `json:"error"`
+	}
+	respBody := returnVals{
+		Error: "Chirp is too long",
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(400)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(400)
+	w.Write(dat)
+	return
+}
+
+func success(w http.ResponseWriter) {
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+	respBody := returnVals{
+		Valid: true,
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(400)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(dat)
+	return
 }
